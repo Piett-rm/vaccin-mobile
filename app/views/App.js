7149a64f382211d4e00f0e43e0358e3d56a4b083
flori <florimond2002@gmail.com>
2022-04-11 10:01:36 +0200

    authent jwt en cours d'implémentation

--- app/views/App.js
+++ app/views/App.js
@@ -1,74 +1,146 @@
 import * as React from 'react'
-import { View, Text, Button } from 'react-native'
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  TouchableOpacity,
+} from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { StatusBar } from 'expo-status-bar'
 import authAPI from '../model/authAPI'
 import PatientsPage from './PatientsPage'
-
-const login = () => {
-  const toto = authAPI.authentificate({
-    username: 'martine39@auger.com',
-    password: 'Epsi.123',
-  })
-  return toto
-}
-
-function HomeScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-      {/* <Button title="login request" onPress={() => console.log(login())} /> */}
-      <Button
-        title="Go to Patient"
-        onPress={() =>
-          navigation.navigate('Patients', { itemId: 16, otherParam: 'Cheese' })
-        }
-      />
-      <Button
-        title="test login request"
-        onPress={() =>
-          console.log(
-            authAPI
-              .authentificate({
-                username: 'martine39@auger.com',
-                password: 'Epsi.123',
-              })
-              .then((result) => console.log(result))
-          )
-        }
-      />
-    </View>
-  )
-}
-
-function DetailsScreen({ route, navigation }) {
-  const { itemId, otherParam } = route.params
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Details Screen</Text>
-      <Text>itemId: {JSON.stringify(itemId)}</Text>
-      <Text>otherParam: {JSON.stringify(otherParam)}</Text>
-      <Button
-        title="Go to Details... again"
-        onPress={() =>
-          navigation.push('Details', {
-            itemId: Math.floor(Math.random() * 100),
-            otherParam: 'Butter',
-          })
-        }
-      />
-      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
-      <Button title="Go back" onPress={() => navigation.goBack()} />
-    </View>
-  )
-}
 
 const Stack = createNativeStackNavigator()
 
 const App = () => {
+  const [jwt, setJwt] = React.useState(null)
+
+  function HomeScreen({ navigation }) {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen</Text>
+        <Button
+          title="Go to Patient"
+          onPress={() =>
+            navigation.navigate('Patients', {
+              itemId: 16,
+              otherParam: 'Cheese',
+            })
+          }
+        />
+        <Button
+          title="Go to Login"
+          onPress={() => navigation.navigate('Login')}
+        />
+        <Button title="Show Jwt" onPress={() => console.log(jwt)} />
+      </View>
+    )
+  }
+
+  const LoginPage = ({ navigation }) => {
+    const [email, setEmail] = React.useState('')
+    const [password, setPassword] = React.useState('')
+
+    const login = () => {
+      try {
+        return authAPI
+          .authentificate({
+            username: 'martine39@auger.com',
+            password: 'Epsi.123',
+          })
+          .then((result) => setJwt(result))
+          .then((result) => console.log(result))
+          .then((axios.defaults.headers['Authorization'] = 'Bearer ' + jwt))
+          .catch((Error) => Error)
+      } catch {
+        return `error`
+      }
+    }
+
+    const styles = StyleSheet.create({
+      container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+
+      image: {
+        marginBottom: 40,
+      },
+
+      inputView: {
+        backgroundColor: '#A4C5F3',
+        borderRadius: 30,
+        width: '70%',
+        height: 45,
+        marginBottom: 20,
+
+        alignItems: 'center',
+      },
+
+      TextInput: {
+        height: 50,
+        flex: 1,
+        padding: 10,
+        marginLeft: 20,
+      },
+
+      forgot_button: {
+        height: 30,
+        marginBottom: 30,
+      },
+
+      loginBtn: {
+        width: '80%',
+        borderRadius: 25,
+        height: 50,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 40,
+        backgroundColor: '#0762D7',
+      },
+    })
+
+    return (
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+        <View style={styles.inputView}>
+          <TextInput
+            style={styles.TextInput}
+            placeholder="Email."
+            placeholderTextColor="#000000"
+            onChangeText={(email) => setEmail(email)}
+          />
+        </View>
+
+        <View style={styles.inputView}>
+          <TextInput
+            style={styles.TextInput}
+            placeholder="Password."
+            placeholderTextColor="#000000"
+            secureTextEntry={true}
+            onChangeText={(password) => setPassword(password)}
+          />
+        </View>
+
+        <TouchableOpacity
+          style={styles.loginBtn}
+          onPress={() => console.log(login())}
+        >
+          <Text style={styles.loginText}>LOGIN</Text>
+        </TouchableOpacity>
+        <Button title="Show Jwt" onPress={() => console.log(jwt)} />
+      </View>
+    )
+  }
   return (
     <NavigationContainer>
       <Stack.Navigator>
+        {!jwt && <Stack.Screen name="Login" component={LoginPage} />}
         <Stack.Screen
           name="Home"
           component={HomeScreen}
